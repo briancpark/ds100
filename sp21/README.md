b76bf3937c133398ff5db5c5635586adee2cea29
Brian Park <briancpark@berkeley.edu>
2021-01-29 23:11:52 -0500

    push lab

--- sp21/README.md
+++
@@ -1,3 +0,0 @@
-# DS-100/sp21
-
-Public facing repo for Data 100, Spring 2021
--- sp21/hw/hw1/tests/q6.py
+++
@@ -1,1 +0,0 @@
-test = {'name': 'q6', 'points': 1, 'suites': [{'cases': [{'code': '>>> type(q6_survey) == str\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
